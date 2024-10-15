6e12965bb51bc249b9610c9ec311084d219309b8
Hellin22 <dno06117@naver.com>
2024-10-15 17:56:36 +0900

    ✨ 기능 추가 / 수정: inbody router 추가

---
+++ healthtartFront/src/router/inbody.js
@@ -0,0 +1,11 @@
+import InbodyRankingPage from "@/views/inbody/InbodyRankingPage.vue";
+
+
+const InbodyRoutes = [
+    {
+        path:"/inbody",
+        component: InbodyRankingPage
+    }
+];
+
+export default InbodyRoutes;
