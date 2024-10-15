8597193ea2d66796f3272a49f0f5d12bfde647ef
YuJeeun <dbwpdms1122@naver.com>
2024-10-15 20:05:47 +0900

    ✨ 기능 추가: RoutineInnerTab.vue 추가

---
+++ healthtartFront/src/components/routine/RoutineInnerTab.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        <nav class="inner-tab">
+            <div class="nav-menu">
+                <button :class="{'active-button': activeTab === 'ai', 'inactive-button': activeTab !== 'ai'}"@click="setActiveTab('ai')">AI 생성 루틴</button>
+            </div>
+            <div class="nav-menu">
+                <button :class="{'active-button': activeTab === 'user', 'inactive-button': activeTab !== 'user'}"@click="setActiveTab('user')">유저 추천 루틴</button>
+            </div>
+        </nav>
+        <div class="main-container">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    import '@/assets/css/RoutineInnerTab.css';
+    import { ref } from 'vue';
+
+    const activeTab = ref('ai'); 
+
+    const setActiveTab = (tab) => {
+        activeTab.value = tab;
+    };
+
+ </script>
