58f2a35f93b8de6562b6d233ff98e6fb3bb3362a
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-10-15 20:07:23 +0900

    ✨ 기능 추가: 운동 기록  달력 components 기능 추가

--- healthtartFront/src/App.vue
+++ healthtartFront/src/App.vue
@@ -1,31 +1,30 @@
 <template>
   <div id="app">
     <AppHeader />
+    <main>
+      <RouterView />
+    </main>
   </div>
-  <main>
-    <RouterView/>
-  </main>
 </template>
 
 <script setup>
 import './assets/css/style.css';
-import {ref, watch} from 'vue';
+import { ref, watch } from 'vue';
 import { RouterView, useRoute } from 'vue-router';
 import AppHeader from '@/components/AppHeader.vue';
 
-  const route = useRoute();
+const route = useRoute();
 
-  const isMainPage = ref(false);
+const isMainPage = ref(false);
+const isHistoryPage = ref(false);
 
-
-  watch(() => route.path, (newPath) => {
-    isMainPage.value = newPath === '/';
-  } ,
-  {
-    immedate: true
-  });
-
+watch(() => route.path, (newPath) => {
+  isMainPage.value = newPath === '/';
+  isHistoryPage.value = newPath === '/history';
+}, {
+  immediate: true  
+});
 </script>
 
-<style setup>
+<style scoped>
 </style>
--- healthtartFront/src/router/router.js
+++ healthtartFront/src/router/router.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import MainPage from '@/views/MainPage.vue';
-import LoginRoutes from './login';
+import HistoryRoutes from '@/router/history/history.js';  // HistoryRoutes로부터 라우트 배열을 가져옴
 
 // function isLoggedIn() {
 //   // localStorage는 vue.js에서 전역으로 사용할 수 있는 storage
@@ -10,7 +10,8 @@ import LoginRoutes from './login';
 // meta: {requireAuth: true} 로그인이 필요한 페이지에 추가하기
 const routes = [
   { path: "/", component: MainPage },
-  ...LoginRoutes
+  ...HistoryRoutes,  // HistoryRoutes 배열을 펼쳐서 추가
+  // ...LoginRoutes, // 필요한 경우 Login 관련 라우트도 추가
 ];
 
 const router = createRouter({
@@ -31,6 +32,5 @@ const router = createRouter({
 //   next();
 // }
 // });
-
 
 export default router;
