0c3ec6092bb2d60f6d11e8278d10366f01951b38
DESKTOP-BC2AP8U\Playdata <cho9759@gmail.com>
2024-10-15 17:44:10 +0900

    ✨ 기능 수정 : mainpage css 수정

--- healthtartFront/src/App.vue
+++ healthtartFront/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <AppHeader />
   </div>
   <main>
     <RouterView/>
@@ -7,9 +8,10 @@
 </template>
 
 <script setup>
-  import './assets/css/style.css';
+import './assets/css/style.css';
 import {ref, watch} from 'vue';
 import { RouterView, useRoute } from 'vue-router';
+import AppHeader from '@/components/AppHeader.vue';
 
   const route = useRoute();
 
