4f148f62db87ab9ca370f9f7d0eeda4f6dd026b4
DESKTOP-BC2AP8U\Playdata <cho9759@gmail.com>
2024-10-16 11:53:08 +0900

    ✨ 기능 추가 : MyPage 라이벌 등록 모달 완성, DeleteModal 추가

--- healthtartFront/src/views/user/MyPage.vue
+++ healthtartFront/src/views/user/MyPage.vue
@@ -7,45 +7,46 @@
       <form class="mypage-form" @submit.prevent="updateProfile">
         <div class="form-group">
           <label for="name">이름</label>
-          <input type="text" id="name" v-model="formData.name" />
+          <input type="text" id="name" v-model="formData.name" readonly />
         </div>
         <div class="form-group">
           <label for="email">이메일</label>
-          <input type="text" id="email" v-model="formData.email" />
+          <input type="text" id="email" v-model="formData.email" readonly />
         </div>
         <div class="form-group">
           <label for="password">비밀번호</label>
-          <input type="password" id="password" v-model="formData.password" />
+          <input type="password" id="password" v-model="formData.password" readonly />
         </div>
         <div class="form-group">
           <label for="phone">전화번호</label>
-          <input type="text" id="phone" v-model="formData.phone" />
+          <input type="text" id="phone" v-model="formData.phone" readonly />
         </div>
         <div class="form-group">
           <label for="nickname">닉네임</label>
-          <input type="text" id="nickname" v-model="formData.nickname" />
+          <input type="text" id="nickname" v-model="formData.nickname" readonly />
         </div>
         <div class="form-group">
           <label for="gender">성별</label>
-          <input type="text" id="gender" v-model="formData.gender" />
+          <input type="text" id="gender" v-model="formData.gender" readonly />
         </div>
         <div class="form-group">
           <label for="height">키</label>
-          <input type="text" id="height" v-model="formData.height" placeholder="?" />
+          <input type="text" id="height" v-model="formData.height" readonly />
         </div>
         <div class="form-group">
           <label for="weight">몸무게</label>
-          <input type="text" id="weight" v-model="formData.weight" placeholder="?" />
+          <input type="text" id="weight" v-model="formData.weight" readonly />
         </div>
         <button class="update-btn">수정</button>
       </form>
       <div class="separator"></div>
+
       <div class="extra-content">
         <div class="extra-section">
           <div class="extra-button-group">
-            <button class="add-gym-btn">등록 헬스장</button>
+            <span class="label-text">등록 헬스장</span>
             <button class="add-btn" @click="openGymModal">추가</button>
-            <button v-if="selectedGym" @click="removeSelectedGym" class="remove-btn">삭제</button>
+            <button v-if="selectedGym" @click="confirmDelete('gym')" class="remove-btn">삭제</button>
           </div>
 
           <div v-if="selectedGym" class="selected-gym">
@@ -64,33 +65,64 @@
 
         <div class="extra-section">
           <div class="extra-button-group">
-            <button class="add-rival-btn">등록 라이벌</button>
-            <button class="add-btn">추가</button>
+            <span class="label-text">등록 라이벌</span>
+            <button class="add-btn" @click="openRivalModal">추가</button>
+            <button v-if="registeredRivals.length > 0" @click="confirmDelete('rival')" class="remove-btn">삭제</button>
+          </div>
+
+          <div v-if="registeredRivals.length > 0" class="selected-rival">
+            <div class="rival-info new-design">
+              <div class="new-design-header-rival">
+                <img src="@/assets/icons/usericon.svg" alt="유저" class="usericon new-design-icon" />
+                <h3 class="new-design-title">{{ registeredRivals[registeredRivals.length - 1] }}</h3>
+              </div>
+              <div class="new-design-actions">
+                <button class="new-design-action-btn">운동기록 보기</button>
+                <button class="new-design-action-btn">인바디 보기</button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </main>
+
     <RightSide />
+
     <RegisterGymModal 
       :isOpen="isGymModalOpen" 
       @close="closeGymModal"
       @selectGym="handleGymSelection"
     />
+    <RegisterRivalModal 
+      :isOpen="isRivalModalOpen" 
+      @close="closeRivalModal"
+      @register="handleRivalRegistration"
+    />
+    <DeleteModal 
+      :isOpen="isDeleteModalOpen"
+      @close="closeDeleteModal"
+      @confirm="deleteItem"
+    />
   </div>
 </template>
-
 
 <script setup>
 import { ref, onMounted } from 'vue';
 import SideMenu from '@/components/SideMenu.vue';
 import RightSide from '@/components/RightSide.vue';
 import BackGround from '@/components/BackGround.vue';
-import RegisterGymModal from '@/components/gym/modal/RegisterGymModal.vue';
+import RegisterGymModal from '@/components/modal/gym/RegisterGymModal.vue';
+import RegisterRivalModal from '@/components/modal/rival/RegisterRivalModal.vue';
+import DeleteModal from '@/components/modal/DeleteModal.vue';
 import '@/assets/css/user/MyPage.css';
 
 const formData = ref(null);
 const isGymModalOpen = ref(false);
+const isRivalModalOpen = ref(false);
+const isDeleteModalOpen = ref(false);
 const selectedGym = ref(null);
+const registeredRivals = ref([]);
+let itemToDelete = ref('');
 
 onMounted(() => {
   formData.value = {
@@ -138,5 +170,38 @@ const showGymEquipment = () => {
 
 const showGymAddress = () => {
   console.log('주소 보기:', selectedGym.value.address);
+};
+
+const openRivalModal = () => {
+  isRivalModalOpen.value = true;
+  document.body.style.overflow = 'hidden';
+};
+
+const closeRivalModal = () => {
+  isRivalModalOpen.value = false;
+  document.body.style.overflow = '';
+};
+
+const handleRivalRegistration = (rivalName) => {
+  registeredRivals.value.push(rivalName);
+  console.log('라이벌 등록: ', rivalName);
+};
+
+const confirmDelete = (itemType) => {
+  itemToDelete.value = itemType;
+  isDeleteModalOpen.value = true;
+};
+
+const closeDeleteModal = () => {
+  isDeleteModalOpen.value = false;
+};
+
+const deleteItem = () => {
+  if (itemToDelete.value === 'gym' && selectedGym.value) {
+    selectedGym.value = null;
+  } else if (itemToDelete.value === 'rival' && registeredRivals.value.length > 0) {
+    registeredRivals.value.pop();
+  }
+  closeDeleteModal();
 };
 </script>
