393154b5fecd35c1d631175629826a16067cf558
YuJeeun <dbwpdms1122@naver.com>
2024-10-17 15:12:09 +0900

    💄 UI/스타일 파일 추가: UserRecommendationPage.css 파일 추가

--- healthtartFront/src/views/routine/UserRecommendationPage.vue
+++ healthtartFront/src/views/routine/UserRecommendationPage.vue
@@ -1,7 +1,15 @@
 <template>
     <div class="user-recommendation">
         <RoutineInnerTab>
-
+            <div class="board-container">
+                <div class="board-header">
+                    <div class="board-header-number">번호</div>
+                    <div class="board-header-title">제목</div>
+                    <div class="board-header-rating">평균 별점</div>
+                    <div class="board-header-time">운동 시간</div>
+                    <div class="board-header-start">시작하기</div>
+                </div>
+            </div>
         </RoutineInnerTab>
     </div>
 </template>
@@ -13,5 +21,13 @@
 </script>
 
 <style scoped>
-
+    .board-header {
+        display: grid;
+        grid-template-columns: 1fr 3fr 1fr 1fr 1fr;
+        padding: 20px 16px;
+        margin-top: -30px;
+        font-weight: bold;
+        border-bottom: 2px solid rgb(1, 254, 174, 0.5);
+        text-align: center;
+    }
 </style>
