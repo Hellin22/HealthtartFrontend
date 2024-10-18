40c1ba210fc3643ca528e95a66b7185c550e7e55
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-10-18 23:45:05 +0900

    ✨ 기능 추가: 만족도 페이지 기능 추가

--- healthtartFront/src/router/routine.js
+++ healthtartFront/src/router/routine.js
@@ -2,11 +2,8 @@ import WorkoutInfoInputPage from "@/views/routine/WorkoutInfoInputPage.vue";
 import UserRecommendationPage from "../views/routine/UserRecommendationPage.vue";
 import UserRecommendationdetailPage from "../views/routine/UserRecommendationdetailPage.vue";
 import AIGenerateRoutinePage from "../views/routine/AIGenerateRoutinePage.vue";
-<<<<<<< HEAD
 import RoutineFinishPage from "../views/routine/RoutineFinish.vue";
-=======
 import StartWorkoutPage from "../views/routine/StartWorkoutPage.vue";
->>>>>>> 701bfaea947a936b38f60377cd0995060a9f7b63
 
 const RoutineRoutes = [
   {
@@ -26,16 +23,13 @@ const RoutineRoutes = [
     component: AIGenerateRoutinePage
   },
   {
-<<<<<<< HEAD
+    path: "/start-workout",
+    component: StartWorkoutPage
+  },
+  {
     path: "/finished-routine",
     component: RoutineFinishPage
   },
-=======
-    path: "/start-workout",
-    component: StartWorkoutPage
-  }
->>>>>>> 701bfaea947a936b38f60377cd0995060a9f7b63
-
 ];
 
 export default RoutineRoutes;
