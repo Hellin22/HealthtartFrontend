18a74daa44d1df1bd0f4e5c4aba69a7fa684aebe
seohyun <694090@gmail.com>
2024-10-18 11:19:42 +0900

    ✨ 기능 추가: OAuth 로그인 시 추가 정보 입력 기능 추가

--- healthtartFront/src/router/users.js
+++ healthtartFront/src/router/users.js
@@ -1,11 +1,13 @@
 import MyPage from "@/views/users/MyPage.vue";
 import MyPageEdit from "@/views/users/MyPageEdit.vue";
 import LoginPage from "@/views/users/LoginPage.vue";
+import AddInfoPage from "../views/users/AddInfoPage.vue";
 
 const UserRoutes = [
     { path: "/mypage", component: MyPage },
     { path: "/mypage/edit", component: MyPageEdit },
-    { path: "/users/login", component: LoginPage }
+    { path: "/users/login", component: LoginPage },
+    { path: "/users/addinfo", component: AddInfoPage}
 ];
 
 export default UserRoutes;
