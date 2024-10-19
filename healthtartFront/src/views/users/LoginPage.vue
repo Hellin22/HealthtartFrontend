8eea248b48640c0b478877c40395620d1eaf7d36
DESKTOP-BC2AP8U\Playdata <cho9759@gmail.com>
2024-10-19 17:04:20 +0900

    ✨ 기능 수정 : App.vue, LoginPage.vue 코드 수정

--- healthtartFront/src/views/users/LoginPage.vue
+++ healthtartFront/src/views/users/LoginPage.vue
@@ -5,15 +5,17 @@
 
 <script setup>
 import { useRouter } from 'vue-router';
+import { inject } from 'vue';
 import LoginBackground from '@/components/users/LoginBackground.vue';
 import LoginForm from '@/components/users/LoginForm.vue';
 
 // Vue Router 사용
 const router = useRouter();
+const loginState = inject('loginState');
 
 // 로그인 성공 처리 함수
 const handleLoginSuccess = (userNickname) => {
-  console.log('로그인 성공, 닉네임:', userNickname);
+  loginState.state.isLoggedIn = true;
   // 로그인 성공 시 메인 페이지로 이동
   router.push({ path: '/' });
   // 필요 시, 추가적인 처리 (로그인 성공 메시지 등)
